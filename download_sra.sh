96a0c0555a078f7dda472f07a14aede1cfd5ccce
Luisa Bender <luisa.bender@tum.de>
2024-09-19 16:51:07 +0200

    added fastqc

--- download_sra.sh
+++ download_sra.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-# Check if the input file is provided
+# SRA Accessions List as input file: SRA_Acc_List.txt
+
+# check if input file is provided
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <accession_list_file>"
     exit 1
 fi
 
-# Read the input file
-ACCESSION_FILE="$1"
+# read in the accession list
+ACCESSION_LIST="$1"
 
 # Check if the file exists
-if [ ! -f "$ACCESSION_FILE" ]; then
-    echo "File not found: $ACCESSION_FILE"
+if [ ! -f "$ACCESSION_LIST" ]; then
+    echo "File not found: $ACCESSION_LIST"
     exit 1
 fi
 
 while IFS= read -r ACC; do
 
-    # if line is not empty
     if [ -n "$ACC" ]; then 
         echo "Downloading $ACC"
     
@@ -42,6 +43,42 @@ while IFS= read -r ACC; do
             echo "Error downloading $ACC."
         fi
     fi
-done < "$ACCESSION_FILE"
+done < "$ACCESSION_LIST"
+
+###### fastqc #######
+
+#perform fastqc on every extracted .fastq file
+
+base_dir="/dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/luisa"
+#output_fastqc="/dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/luisa/fastqc_results"
+
+#mkdir "$output_fastqc"
+
+#for dir in "$base_dir"/*/"fastq"/; do
+ #   echo "Processing directory: $dir"
+
+    # find all fastq files
+  #  for fastq_file in "$dir"*.fastq; do
+   #     if [ -f "$fastq_file" ]; then
+    #        echo "Running FastQC on $fastq_file"
+     #       fastqc "$fastq_file" --outdir="$output_fastqc"
+      #  fi
+    #done
+#done
+
+### adapter trimming with fastp ####
+
+
+for dir in "$base_dir"/*/"fastq"/; do
+    echo "Processing directory: $dir"
+
+    for fastq_file in "$dir"*.fastq; do
+        if [ -f "$fastq_file" ]; then
+            echo "Running fastp on $fastq_file"
+            fastp -i "$fastq_file" -o "$fastq_file"_trimmed
+        fi
+    done
+done
+
 
 
