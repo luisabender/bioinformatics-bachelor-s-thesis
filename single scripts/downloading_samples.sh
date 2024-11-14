ecf57ee1d33fe418eb9de67af90de19d675473f9
Luisa Bender <luisa.bender@tum.de>
2024-11-14 12:23:16 +0100

    structure

---
+++ single scripts/downloading_samples.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+
+base_dir="/dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/luisa"
+
+# SRA Accessions List as input file: SRA_Acc_List.txt
+
+# check if input file is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <accession_list_file>"
+    exit 1
+fi
+
+# read in the accession list
+ACCESSION_LIST="$1"
+echo $ACCESSION_LIST
+
+# Check if file exists
+if [ ! -f "$ACCESSION_LIST" ]; then
+    echo "File not found: $ACCESSION_LIST"
+    exit 1
+fi
+
+cd $base_dir/samples/
+echo "changed to directory samples"
+
+while IFS= read -r ACC; do
+
+    if [ -n "$ACC" ]; then 
+        echo "Downloading $ACC"
+    
+        # download the SRA file
+        prefetch "$ACC"
+    
+        if [ $? -eq 0 ]; then
+            echo "$ACC downloaded successfully."
+            echo "Extracting $ACC"
+        
+            # Extract fastq file from .sra file
+            cd "$ACC"
+            fasterq-dump --outdir fastq "$ACC" 
+            
+            if [ $? -eq 0 ]; then
+                echo "$ACC extracted successfully."
+                cd ".."
+            else
+                echo "Error extracting $ACC."
+                #cd ".."
+            fi
+        else
+            echo "Error downloading $ACC."
+        fi
+    fi
+done < "$ACCESSION_LIST"
+
+cd $base_dir
