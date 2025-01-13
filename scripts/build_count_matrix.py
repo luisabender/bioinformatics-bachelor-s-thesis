7c8bd039fce1fe2dbfceafe1d2c3fdaf43f9606c
Luisa Bender <luisa.bender@tum.de>
2025-01-13 11:49:25 +0100

    counts tpm

--- scripts/build_count_matrix.py
+++ scripts/build_count_matrix.py
@@ -16,10 +16,10 @@ for sample in sample_dirs_single:
     abundance_df_single = pd.read_csv(tsv_file_single, sep="\t")
     
     # extract target ID (1st column) and tpm
-    sample_counts_single = abundance_df_single[['target_id', 'est_counts']].copy()
+    sample_counts_single = abundance_df_single[['target_id', 'tpm']].copy()
     
     # tpm = name of sample
-    sample_counts_single.rename(columns={'est_counts': sample}, inplace=True)
+    sample_counts_single.rename(columns={'tpm': sample}, inplace=True)
     
     if count_matrix_single.empty:
         count_matrix_single = sample_counts_single
@@ -35,10 +35,10 @@ for sample in sample_dirs_paired:
     abundance_df_paired = pd.read_csv(tsv_file_paired, sep="\t")
     
     # extract target ID (1st column) and tpm
-    sample_counts_paired = abundance_df_paired[['target_id', 'est_counts']].copy()
+    sample_counts_paired = abundance_df_paired[['target_id', 'tpm']].copy()
     
     # tpm = name of sample
-    sample_counts_paired.rename(columns={'est_counts': sample}, inplace=True)
+    sample_counts_paired.rename(columns={'tpm': sample}, inplace=True)
     
     if count_matrix_paired.empty:
         count_matrix_paired = sample_counts_paired
@@ -51,4 +51,4 @@ count_matrix_paired.set_index('target_id', inplace=True)
 count_matrix = pd.merge(count_matrix_single, count_matrix_paired, left_index=True, right_index=True, how="outer")
 
 # save as csv file
-count_matrix.to_csv("/dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/luisa/bachelor-thesis-luisa/count_tables/count_matrix_raw.csv")
+count_matrix.to_csv("/dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/luisa/bachelor-thesis-luisa/count_tables/counts_tpm.csv")
