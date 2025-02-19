4ca61745f9ef9bf25ea0d502c1fb6d3f7257d966
Luisa Bender <luisa.bender@tum.de>
2025-02-19 17:30:37 +0100

    network construction with power 4,5,6

--- scripts/WGCNA_netconstr.R
+++ scripts/WGCNA_netconstr.R
@@ -1,18 +1,23 @@
 # WGCNA adjacency with different soft thresholds
 library(WGCNA)
 # load expression data
-base_dir <- "/dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/luisa/bachelor-thesis-luisa/"
+base_dir <- "/dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/luisa/bachelor-thesis-luisa"
 options(stringsAsFactors = FALSE)
 enableWGCNAThreads()
 
-expr_data <- read.csv(file.path(base_dir,"count_tables/vst_expression_data.csv"), row.names = 1)
+expr_data <- read.csv(file.path(base_dir,"count_tables/expr_data.csv"), row.names = 1)
 
 # transpose it for WGCNA
 input_mat <- t(expr_data)
 
+powers = c(4,5,6)
+
 # change the adjacancy into signed hybrid
+for (power in powers){
+
+print(paste("Starting with power ", power))
 print("Running adjacency...")
-softPower <- 4
+softPower <- power
 adjacency <- adjacency(input_mat, power = softPower, type = "signed hybrid")
 
 # Topological Overlap Matrix
@@ -43,7 +48,8 @@ METree = hclust(as.dist(ME.dissimilarity), method = "average") #Clustering eigen
 par(mar = c(0,4,2,0)) #seting margin sizes
 par(cex = 0.6);#scaling the graphic
 
-pdf(file.path(base_dir,"/wgcna_results/METree_sp4_signed.pdf"))
+pdf(file.path(base_dir, "wgcna_results", "new", paste0("METree_sp", power, "_signed.pdf")))
+
 plot(METree)
 abline(h=.25, col = "red") #a height of .25 corresponds to correlation of .75
 dev.off()
@@ -57,7 +63,7 @@ mergedColors <- merge$colors
 mergedMEs <- merge$newMEs
 
 # plot dendrogram of original and merged module colors
-pdf(file.path(base_dir,"/wgcna_results/dendro_merged_sp4_signed.pdf"))
+pdf(file.path(base_dir, "wgcna_results", "new",paste0("dendro_merged_sp",power,"_signed.pdf")))
 dendro_merged <- plotDendroAndColors(geneTree, 
                     cbind(ModuleColors, mergedColors),
                     c("original Module", "merged Module"),
@@ -69,7 +75,8 @@ dendro_merged <- plotDendroAndColors(geneTree,
 dev.off()
 
 
-save(mergedMEs, mergedColors, geneTree, file = file.path(base_dir,"/wgcna_results/networkConstruction-pow4_signed.RData"))
+save(mergedMEs, mergedColors, geneTree, file = file.path(base_dir, "wgcna_results", "new",paste0("networkConstruction-pow",power,"_signed.RData")))
 
 
-print("Finished succesfully")
+'+print(paste("Finished succesfully with power", power))
+}
